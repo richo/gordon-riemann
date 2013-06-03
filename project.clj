5d392b754bfc8a72ce4d1e9d0bf0e9cd7bee7494
Richo Healey <richo@psych0tik.net>
2013-06-03 14:13:14 +1000

    Include scala dep and project

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject edda-stream "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [com.novemberain/monger "1.5.0"]
-                 [com.amazonaws/aws-java-sdk "1.3.11" :exclusions [org.codehaus.jackson/jackson-mapper-asl]]])
+                 [com.amazonaws/aws-java-sdk "1.3.11" :exclusions [org.codehaus.jackson/jackson-mapper-asl]]
+                 [org.scala-lang/scala-library "2.9.1"]]
+  :source-paths ["edda/build/classes/main/"]
+  )
