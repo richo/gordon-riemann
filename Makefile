27d09aecd6fcc466679630e8781263dc90897cc2
Stuart Campbell <stuart@harto.org>
2013-06-03 16:14:09 +1000

    Slightly clean up deps

--- Makefile
+++ Makefile
@@ -1,13 +1,20 @@
+.PHONY: clean
+
 all: lib
 
 lib: edda/build/exploded
-	mkdir -p lib; find edda/build/exploded -name \*.jar -exec cp {} lib \;
+	mkdir -p lib
+	find edda/build/exploded -name \*.jar -exec cp {} lib \;
+	cp edda/build/libs/edda-2.1-SNAPSHOT.jar lib
 
-edda/build/exploded: edda/build/classes
+edda/build/exploded: edda/build/libs
 	mkdir -p edda/build/exploded; cd edda/build/exploded; jar -xf ../libs/edda-2.1-SNAPSHOT.war
 
-edda/build/classes: edda
+edda/build/libs: edda
 	cd edda; ./gradlew build
 
 edda:
 	git submodule update --init
+
+clean:
+	rm -rf lib edda/build
--- project.clj
+++ project.clj
@@ -1,10 +1,6 @@
 (defproject edda-stream "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [com.novemberain/monger "1.6.0-beta2"]
-                 [com.amazonaws/aws-java-sdk "1.3.11" :exclusions [org.codehaus.jackson/jackson-mapper-asl]]
-                 [org.scala-lang/scala-library "2.10.1"]
-                 [clj-time "0.5.1"]]
-  :source-paths ["edda/build/libs/edda-2.1-SNAPSHOT.jar"
-                 "lib/*"
-                 "src/"]
-  )
+                 [org.scala-lang/scala-library "2.10.1"]]
+  :source-paths ["lib/*"
+                 "src/"])
