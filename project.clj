59a39d083f46735f746434a75359792b90e4b245
Richo Healey <richo@psych0tik.net>
2013-06-03 15:24:58 +1000

    Include our code in classpath

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
                  [com.amazonaws/aws-java-sdk "1.3.11" :exclusions [org.codehaus.jackson/jackson-mapper-asl]]
                  [org.scala-lang/scala-library "2.10.1"]
                  [clj-time "0.5.1"]]
-  :source-paths ["edda/build/libs/edda-2.1-SNAPSHOT.jar"]
+  :source-paths ["edda/build/libs/edda-2.1-SNAPSHOT.jar" "src/"]
   )
