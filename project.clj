557e30f39c10bb09b72aea15caf96ddbbf3588c5
Stuart Campbell <stuart@harto.org>
2013-06-17 16:51:57 +1000

    Logging configuration

--- project.clj
+++ project.clj
@@ -1,7 +1,13 @@
 (defproject edda-stream "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.scala-lang/scala-library "2.10.1"]
-                 [riemann-clojure-client "0.2.6"]]
-  :source-paths ["lib/*"
-                 "src/"]
+                 [riemann-clojure-client "0.2.6"]
+                 ;; Logging
+                 [org.clojure/tools.logging "0.2.6"]
+                 [org.slf4j/slf4j-api "1.6.6"]
+                 [org.slf4j/slf4j-log4j12 "1.7.5"]
+                 [log4j/log4j "1.2.17"]]
+  :source-paths ["resources" ; This must be first because Reasons.
+                 "lib/*"
+                 "src"]
   :main edda-stream.main)
