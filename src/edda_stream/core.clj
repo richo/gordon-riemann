fd6187f5a6b0387642077db01fdd6382f8384f66
Richo Healey <richo@psych0tik.net>
2013-06-17 13:03:52 +1000

    Update finder method

--- src/edda_stream/core.clj
+++ src/edda_stream/core.clj
@@ -1,12 +1,15 @@
 (ns edda-stream.core
-  (:require [edda-stream.mongo :as m]
-            [edda-stream.scala :as s])
-  (:import [com.netflix.edda.mongo MongoDatastore]))
+  (:require [edda-stream.scala :as s])
+  (:import [com.netflix.edda.mongo MongoDatastore]
+           [org.joda.time DateTime]
+           [scala.collection JavaConversions]))
 
-(defn find-thing []
-  (let [datastore (MongoDatastore. "aws.instances")]
+
+
+(defn find-thing [coll where & [limit]]
+  (let [datastore (MongoDatastore. coll)]
     (.query datastore
-            (s/clj->scala {"_id" ""}); FIXME Insert "{reservation_id}|{timestamp}"
-            0
+            (s/clj->scala where)
+            (or limit 0)
             (s/clj->scala #{})
             true)))
