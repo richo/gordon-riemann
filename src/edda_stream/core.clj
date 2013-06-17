c13931dc36f23abb8ead5f13f3320925701c6ad6
Stuart Campbell <stuart@harto.org>
2013-06-17 13:38:19 +1000

    Riemann client helper

--- src/edda_stream/core.clj
+++ src/edda_stream/core.clj
@@ -1,10 +1,12 @@
 (ns edda-stream.core
-  (:require [edda-stream.scala :as s])
+  (:require [edda-stream.scala :as s]
+            [riemann.client :as r])
   (:import [com.netflix.edda.mongo MongoDatastore]
            [org.joda.time DateTime]
            [scala.collection JavaConversions]))
 
-
+(defonce riemann-client
+  (memoize (fn [] (r/tcp-client :host "127.0.0.1"))))
 
 (defn find-thing [coll where & [limit]]
   (let [datastore (MongoDatastore. coll)]
