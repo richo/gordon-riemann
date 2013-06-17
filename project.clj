db6ef9d7c26ef4e906c107d65f863b357a870c28
Stuart Campbell <stuart@harto.org>
2013-06-17 13:39:22 +1000

    Remove Mongo cruft
    Mongo interactions now happen via edda's API.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject edda-stream "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [com.novemberain/monger "1.6.0-beta2"]
                  [org.scala-lang/scala-library "2.10.1"]
                  [riemann-clojure-client "0.2.6"]]
   :source-paths ["lib/*"
