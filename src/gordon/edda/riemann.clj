424098c16cff12ac48fc8837561d17929f319a8e
Richo Healey <richo@psych0tik.net>
2013-07-01 12:15:26 +1000

    Push instance events to riemann

--- src/gordon/edda/riemann.clj
+++ src/gordon/edda/riemann.clj
@@ -5,20 +5,30 @@
 (defonce riemann-client
   (memoize (fn [] (r/tcp-client :host "127.0.0.1"))))
 
+(defn instance->event [instance]
+  {:host (get-instance-name data) :service (get-instance-role data)
+   :state (get-instance-state data) :metric (state->metric (get-instance-state data))})
+
 (defn get-instance-state [instance]
-  (case (get-in (first (get instance "instances")) ["state" "name"])
-    "running" "1"
-    "terminated" "0"
-    "stopping" "0"
-    "stopped" "0"
-    "shutting-down" "0"
-    nil))
+  (get-in (first (get instance "instances")) ["state" "name"]))
 
 (defn get-instance-name [instance]
   (get (first (get instance "instances")) "instanceId"))
 
+; TODO
+(defn get-instance-role [instance]
+  (get (first (get instance "instances")) "instanceId"))
+
+
+(defn state->metric [state]
+  (case state
+        "running" 0
+        "terminated" 1
+        "stopping" 2
+        "stopped" 4
+        "shutting-down" 8))
+
 (defn create-event [ev]
   (let [data (s/scala->clj (.data (s/scala->clj ev)))]
     (r/send-event (riemann-client)
-                  {:service (get-instance-name data)
-                   :state (get-instance-state data)})))
+                  (instance->event data))))
