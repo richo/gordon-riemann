be0a08bb2b0539eed817d868060f319b7f1effcc
Richo Healey <richo@psych0tik.net>
2013-07-26 16:44:16 -0700

    Refactor the single test to reflect the new api

--- test/gordon/test/riemann/core.clj
+++ test/gordon/test/riemann/core.clj
@@ -2,14 +2,18 @@
   (:use [gordon.riemann.core :as c]
         [clojure.test]))
 
-; Set handlers to three identity functions
-(defn id [value]
-  value)
+; Create a handler that atomically bumps a value
+(defn bump-value
+  [value]
+  (fn [_]
+    (swap! value (fn [old] (+ old 1)))))
 
-(c/add-handler id)
-(c/add-handler id)
-(c/add-handler id)
+(let [test-value (atom 0)]
+  (c/add-handler (bump-value test-value))
+  (c/add-handler (bump-value test-value))
+  (c/add-handler (bump-value test-value))
 
-(deftest runs-all-handlers
-  (is (= (c/handle-event 1)
-      [1 1 1])))
+  (deftest runs-all-handlers
+    (c/handle-event nil)
+    (is (= @test-value
+           3))))
