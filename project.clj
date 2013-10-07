5393a4daa257e78aedbf1f7ce1ccb574b5ba0875
Richo Healey <richo@psych0tik.net>
2013-10-07 15:08:40 +1100

    Fix name of project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject edda-stream "0.1.0-SNAPSHOT"
+(defproject gordon-riemann "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.scala-lang/scala-library "2.10.1"]
                  [riemann-clojure-client "0.2.6"]
